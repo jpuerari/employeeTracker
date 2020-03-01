91ad8e17b3ddc24673c8047c4149318b4873fbb2
Joseph D. Puerari <joep@Joes-MBP.fios-router.home>
2020-03-01 17:29:49 -0500

    updated seeds and prompts

--- db/schemas.sql
+++ db/schemas.sql
@@ -3,3 +3,4 @@ DROP DATABASE IF EXISTS employeeTracker_db;
 CREATE DATABASE employeeTracker_db;
 
 USE employeeTracker_db;
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,24 @@
 USE employeeTracker_db;
 
-INSERT INTO items
-  (item_name, item_category)
+INSERT INTO department
+(name)
 VALUES
+  ('Localization Engineer'),
+  ('In-house Translator')
+  ('Project Manager')
+  ('Clerk');
+
+  INSERT INTO role
+  (title, salary, department_id)
+  VALUES
+  ('Senior Localization Engineer', 100000, 1),
+  ('In-house Spanish>English Translator', 60000, 2),
+  ('Translation Project Manager', 50000, 3),
+  ('Desk Clerk', 40000, 4);
+
+  INSERT INTO employees
+  (first_name, last_name, role_id),
+  ('Mark', 'Anthony', 1),
+  ('Allen' 'Smith', 2),
+  ('Jane' 'Doe', 3),
+  ('Carry' 'Johnson', 4);
